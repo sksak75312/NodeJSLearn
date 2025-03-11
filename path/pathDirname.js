1620ae6e283ccd52b4d51f44b0ba7f0eb80f0d4b
EeroChiao <eero.chiao@udngroup.com.tw>
2025-03-11 14:17:05 +0800

    wip: Path dirname

---
+++ path/pathDirname.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+console.log(path.dirname('C:\\Users\\07345\\Desktop\\personal\\NodeJSLearn\\path\\pathBasename.js'));
+
+// output: C:\Users\07345\Desktop\personal\NodeJSLearn\path
