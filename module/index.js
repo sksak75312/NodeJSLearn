27bae18ae2d951b555fa3e625301763a2e7491f1
EeroChiao <eero.chiao@udngroup.com.tw>
2025-03-12 15:07:34 +0800

    wip: Module exports

---
+++ module/index.js
@@ -0,0 +1,4 @@
+const me = require('./me.js');
+
+me.hand()
+me.head()
---
+++ module/me.js
@@ -0,0 +1,15 @@
+function head() {
+  console.log('head');
+}
+
+function hand() {
+  console.log('hand');
+}
+
+// module.exports = {
+//   head,
+//   hand,
+// };
+
+exports.head = head;
+exports. hand = hand;
